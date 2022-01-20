20c9ff0057a8daf5e62abef7f3b334550e4dadff
Hannah Bush <heb.rutgers@gmail.com>
2022-01-20 00:07:31 -0500

    added more js

---
+++ js/game.js
@@ -0,0 +1,10 @@
+// ask player for robot's name
+var playerName = window.prompt("What is your robot's name?");
+// this restates the entered name
+console.log(playerName);
+// this creates a function named "fight"
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+//fight();
