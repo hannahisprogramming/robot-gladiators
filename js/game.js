36dff0b506902544d8002131fe3ab4546a490899
Hannah Bush <heb.rutgers@gmail.com>
2022-01-21 10:09:16 -0500

    mvp finished

--- js/game.js
+++ js/game.js
@@ -1,10 +1,50 @@
-// ask player for robot's name
 var playerName = window.prompt("What is your robot's name?");
-// this restates the entered name
-console.log(playerName);
-// this creates a function named "fight"
-function fight() {
-  window.alert("The fight has begun!");
+var playerHealth = 100;
+var playerAttack = 50;
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+var playerAttackOutcome = function() {
+  enemyHealth-=playerAttack;
+  window.alert(enemyHealth);
+}
+// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
+var enemyAttackOutcome = function() {
+  playerHealth-=enemyAttack;
+  window.alert(playerHealth);
 }
 
-//fight();
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+  playerAttackOutcome();
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+  enemyAttackOutcome();
+  // check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+};
+
+fight();
+
+// Log a resulting message to the console so we know that it worked.
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+// Log a resulting message to the console so we know that it worked.
+console.log(
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
